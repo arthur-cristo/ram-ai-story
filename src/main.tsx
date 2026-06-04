923f7820e56867fc9d23deeecd656bc9d9c955c2
Arthur Cristo <arthur.cristo.dev@gmail.com>
2026-06-04 19:22:51 -0300

    dep: chakra:typegen

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App'
-import { system } from './theme'
+import { system } from './styles/system'
 
 import './index.css'
 
